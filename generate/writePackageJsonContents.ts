6affbf5d9ced35010f0d60c8890cdf701dbd21c7
Bogdan Matei <bogdan.matei@grafana.com>
2024-04-01 20:48:25 +0300

    Fix writing the package.json file

--- generate/writePackageJsonContents.ts
+++ generate/writePackageJsonContents.ts
@@ -8,6 +8,7 @@ import { Command } from "./types";
 
 export function writePackageJsonContents(commands: Command[]): void {
   if (!isEqual(packageJson.commands, commands)) {
+    // @ts-expect-error: There is a mismatch between the types in package.json and the types in the script. Nothing to worry about to be honest
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     packageJson.commands = commands.map(({ parameter: _parameter, ...command }) => command);
 
